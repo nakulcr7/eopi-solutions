9c3d887bd25e03e5c49d7c5fe303e75ce99c4886
Nakul Camasamudram <nakulcr7@fb.com>
2021-04-14 22:57:11 -0700

    problems 5.1 and 5.6

--- eopi_solutions/arrays.py
+++ eopi_solutions/arrays.py
@@ -1,5 +1,47 @@
 #!/usr/bin/env python3
+from typing import List
 
 
-def hello_world() -> str:
-    return "hello, world"
+"""
+5.1. Write a program that takes an array `A` and index `i` into `A`, and rearrages
+the elements such that all elements less than `A[i]`(the pivot) appear first,
+followed by elements equal to the pivot, followed by elements greater than the pivot.
+"""
+
+def partition_array(A: List[int], i: int) -> List[int]:
+    smaller, equal, larger = 0, 0, len(A)
+    pivot = A[i]
+
+    # Maintaining the below invariants.
+    # - A[:smaller]: <A[i]
+    # - A[smaller:equal]: =A[i]
+    # - A[equal:larger]: unprocessed
+    # - A[larger:]: >A[i]
+
+    while equal < larger:
+        if A[equal] < pivot:
+            A[smaller], A[equal] = A[equal], A[smaller]
+            smaller += 1
+            equal += 1
+        elif A[equal] == pivot:
+            equal += 1
+        else:
+            larger -= 1
+            A[larger], A[equal] = A[equal], A[larger]
+    
+    return A
+
+
+"""
+5.6. Write a program that takes an array denoting the daily stock price, and
+returns the maximum profit that could be made by buying and then selling
+one share of that stock. There is no need to buy if no profit is possible.
+"""
+
+def trade_single_stock(stock_prices: List[int]) -> int:
+    min_price, profit = float("inf"), 0
+    for price in stock_prices:
+        profit = max(profit, price - min_price)
+        min_price = min(min_price, price)
+    return profit
+
--- eopi_solutions/tests/test_arrays.py
+++ eopi_solutions/tests/test_arrays.py
@@ -4,5 +4,17 @@ from eopi_solutions import arrays
 
 
 class TestArrays(unittest.TestCase):
-    def test_hello_world(self) -> None:
-        self.assertEqual(arrays.hello_world(), "hello, world")
+    def test_partition_array(self) -> None:
+        self.assertListEqual(arrays.partition_array([1, 3, 2], 2), [1, 2, 3])
+        self.assertListEqual(arrays.partition_array([3, 2, 1], 1), [1, 2, 3])
+        
+        # one possible version: [1, 2, 2, 3, 3, 4, 5]
+        partitioned = arrays.partition_array([4, 3, 3, 5, 2, 2, 1], 2)
+        self.assertTrue(all(i < 3 for i in partitioned[:3]))
+        self.assertTrue(all(i == 3 for i in partitioned[3:4]))
+        self.assertTrue(all(i > 3 for i in partitioned[5:]))
+
+    def test_trade_single_stock(self) -> None:
+        self.assertEqual(arrays.trade_single_stock([1, 2, 3]), 2)
+        self.assertEqual(arrays.trade_single_stock([3, 2, 1]), 0)
+        self.assertEqual(arrays.trade_single_stock([5, 1, 3, 2, 4, 1]), 3)
